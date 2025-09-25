61b802e9463f00f16a8a70e7c0816ceba956a986
Peter Palaga <ppalaga@redhat.com>
2025-09-25 19:31:07 +0200

    Fetch rather than clone if the working copy exists, perform git I/O on a background thread and in parallel, if there are multiple git repos.

--- domino/build-recipes/src/main/java/com/redhat/hacbs/recipes/location/RecipeRepositoryManager.java
+++ domino/build-recipes/src/main/java/com/redhat/hacbs/recipes/location/RecipeRepositoryManager.java
@@ -3,25 +3,45 @@ package com.redhat.hacbs.recipes.location;
 import com.redhat.hacbs.recipes.util.GitCredentials;
 import java.io.IOException;
 import java.io.UncheckedIOException;
+import java.nio.file.AccessDeniedException;
+import java.nio.file.DirectoryStream;
+import java.nio.file.FileSystemException;
+import java.nio.file.FileVisitResult;
 import java.nio.file.Files;
 import java.nio.file.Path;
+import java.nio.file.SimpleFileVisitor;
+import java.nio.file.attribute.BasicFileAttributes;
 import java.time.Duration;
 import java.util.List;
 import java.util.Optional;
+import java.util.Set;
+import java.util.concurrent.CompletableFuture;
+import java.util.concurrent.ExecutionException;
+import java.util.concurrent.Future;
+import java.util.concurrent.atomic.AtomicInteger;
 import org.eclipse.jgit.api.Git;
+import org.eclipse.jgit.api.ResetCommand.ResetType;
 import org.eclipse.jgit.api.errors.GitAPIException;
+import org.eclipse.jgit.lib.Ref;
+import org.eclipse.jgit.lib.StoredConfig;
 import org.eclipse.jgit.merge.ContentMergeStrategy;
 import org.eclipse.jgit.merge.MergeStrategy;
+import org.eclipse.jgit.transport.FetchResult;
+import org.jboss.logging.Logger;
 
 /**
  * A recipe database stored in git.
  */
 public class RecipeRepositoryManager implements RecipeDirectory {
+    private static final Logger log = Logger.getLogger(RecipeRepositoryManager.class);
+
     public static final String SCM_INFO = "scm-info";
     public static final String BUILD_INFO = "build-info";
     public static final String BUILD_TOOL_INFO = "build-tool-info";
     public static final String REPOSITORY_INFO = "repository-info";
     public static final String DISABLED_PLUGINS = "disabled-plugins";
+    private static final String DOMINO_WORKING_BRANCH = "domino-working-branch";
+
     private final Git git;
     private final String remote;
     private final Path local;
@@ -40,14 +60,14 @@ public class RecipeRepositoryManager implements RecipeDirectory {
         this.recipeLayoutManager = new RecipeLayoutManager(local);
     }
 
-    public static RecipeRepositoryManager create(String remote)
+    public static RecipeDirectory create(String remote)
             throws GitAPIException, IOException {
         return create(remote, Optional.empty(), Files.createTempDirectory("recipe"));
     }
 
-    public static RecipeRepositoryManager create(String remote, Optional<Duration> updateInterval,
+    public static RecipeDirectory create(String remote, Optional<Duration> updateInterval,
             Path directory) throws GitAPIException, IOException {
-        //Allow cloning of another branch via <url>#<branch> format.
+        // Allow cloning of another branch via <url>#<branch> format.
         String branch = "main";
         int b = remote.indexOf('#');
         if (b > 0) {
@@ -55,24 +75,43 @@ public class RecipeRepositoryManager implements RecipeDirectory {
             remote = remote.substring(0, b);
         }
         return create(remote, branch, updateInterval, directory);
-
     }
 
-    public static RecipeRepositoryManager create(String remote, String branch, Optional<Duration> updateInterval,
+    private static AtomicInteger threadIndex = new AtomicInteger();
+
+    public static RecipeDirectory create(
+            String remote,
+            String branch,
+            Optional<Duration> updateInterval,
             Path directory) throws GitAPIException {
 
-        var clone = Git.cloneRepository()
-                .setBranch(branch)
-                .setDirectory(directory.toFile())
-                .setCredentialsProvider(new GitCredentials())
-                .setDepth(1)
-                .setURI(remote);
-        var result = clone.call();
-
-        return new RecipeRepositoryManager(result, remote, directory, branch, updateInterval);
+        final CompletableFuture<RecipeDirectory> delegate = new CompletableFuture<>();
+        new Thread(() -> {
+            try {
+                final Git git;
+                if (Files.exists(directory.resolve(".git"))) {
+                    /* fetch and reset */
+                    git = openGit(directory);
+                    fetchAndReset(remote, branch, git);
+                } else {
+                    /* Shallow clone */
+                    git = Git.cloneRepository()
+                            .setBranch(branch)
+                            .setDirectory(directory.toFile())
+                            .setCredentialsProvider(new GitCredentials())
+                            .setDepth(1)
+                            .setURI(remote)
+                            .call();
+                }
+                delegate.complete(new RecipeRepositoryManager(git, remote, directory, branch, updateInterval));
+            } catch (Exception e) {
+                delegate.completeExceptionally(e);
+            }
+        }, "git-fetch-" + threadIndex.incrementAndGet()).start();
+        return new LazyRecipeDirectory(delegate);
     }
 
-    public static RecipeRepositoryManager createLocal(Path directory) throws GitAPIException {
+    public static RecipeDirectory createLocal(Path directory) throws GitAPIException {
         if (!Files.isDirectory(directory)) {
             throw new IllegalArgumentException(directory + " is not a valid directory");
         }
@@ -162,4 +201,244 @@ public class RecipeRepositoryManager implements RecipeDirectory {
                 ", lastUpdate=" + lastUpdate +
                 '}';
     }
+
+    static Git openGit(Path dir) {
+        try {
+            return Git.open(dir.toFile());
+        } catch (IOException e) {
+            log.debug("No git repository in %s", dir, e);
+        }
+        try {
+            ensureDirectoryExistsAndEmpty(dir);
+            return Git.init().setDirectory(dir.toFile()).call();
+        } catch (IOException | GitAPIException e) {
+            throw new RuntimeException("Could not open git repository in " + dir, e);
+        }
+    }
+
+    static String fetchAndReset(String useUrl, String branch, Git git) {
+        final Path dir = git.getRepository().getWorkTree().toPath();
+        /* Forget local changes */
+        try {
+            Set<String> removedFiles = git.clean().setCleanDirectories(true).call();
+            if (!removedFiles.isEmpty()) {
+                log.warnf("Removed unstaged files %s", removedFiles);
+            }
+            git.reset().setMode(ResetType.HARD).call();
+        } catch (Exception e) {
+            log.warnf(e, "Could not forget local changes in %s", dir);
+        }
+
+        log.infof("Fetching from %s", useUrl);
+        final String remoteAlias = "origin";
+        try {
+            ensureRemoteAvailable(useUrl, remoteAlias, git);
+
+            final String remoteRef = "refs/heads/" + branch;
+            final FetchResult fetchResult = git.fetch().setRemote(remoteAlias).setRefSpecs(remoteRef).call();
+            final String remoteHead = fetchResult.getAdvertisedRef(remoteRef).getObjectId().getName();
+            log.infof("Reseting the working copy to %s", remoteHead);
+            /* Reset the domino-working-branch */
+            git.branchCreate().setName(DOMINO_WORKING_BRANCH).setForce(true).setStartPoint(remoteHead).call();
+            git.checkout().setName(DOMINO_WORKING_BRANCH).call();
+            git.reset().setMode(ResetType.HARD).setRef(remoteHead).call();
+            final Ref ref = git.getRepository().exactRef("HEAD");
+            return ref.getObjectId().getName();
+        } catch (IOException | GitAPIException e) {
+            throw new RuntimeException("Could not fetch and reset " + dir + " from " + useUrl, e);
+        }
+    }
+
+    static void ensureRemoteAvailable(String useUrl, String remoteAlias, Git git) throws IOException {
+        final StoredConfig config = git.getRepository().getConfig();
+        boolean save = false;
+        final String foundUrl = config.getString("remote", remoteAlias, "url");
+        if (!useUrl.equals(foundUrl)) {
+            config.setString("remote", remoteAlias, "url", useUrl);
+            save = true;
+        }
+        final String foundFetch = config.getString("remote", remoteAlias, "fetch");
+        final String expectedFetch = "+refs/heads/*:refs/remotes/" + remoteAlias + "/*";
+        if (!expectedFetch.equals(foundFetch)) {
+            config.setString("remote", remoteAlias, "fetch", expectedFetch);
+            save = true;
+        }
+        if (save) {
+            config.save();
+        }
+    }
+
+    private static final boolean isWindows = System.getProperty("os.name").toLowerCase().contains("win");
+    private static final long DELETE_RETRY_MILLIS = 5000L;
+    private static final int CREATE_RETRY_COUNT = 256;
+
+    /**
+     * If the given directory does not exist, creates it using {@link #ensureDirectoryExists(Path)}. Otherwise
+     * recursively deletes all subpaths in the given directory.
+     *
+     * @param dir the directory to check
+     * @throws IOException if the directory could not be created, accessed or its children deleted
+     */
+    public static void ensureDirectoryExistsAndEmpty(Path dir) throws IOException {
+        if (Files.exists(dir)) {
+            try (DirectoryStream<Path> subPaths = Files.newDirectoryStream(dir)) {
+                for (Path subPath : subPaths) {
+                    if (Files.isDirectory(subPath)) {
+                        deleteDirectory(subPath);
+                    } else {
+                        Files.delete(subPath);
+                    }
+                }
+            }
+        } else {
+            ensureDirectoryExists(dir);
+        }
+    }
+
+    /**
+     * Makes sure that the given directory exists. Tries creating {@link #CREATE_RETRY_COUNT} times.
+     *
+     * @param dir the directory {@link Path} to check
+     * @throws IOException if the directory could not be created or accessed
+     */
+    public static void ensureDirectoryExists(Path dir) throws IOException {
+        Throwable toThrow = null;
+        for (int i = 0; i < CREATE_RETRY_COUNT; i++) {
+            try {
+                Files.createDirectories(dir);
+                if (Files.exists(dir)) {
+                    return;
+                }
+            } catch (AccessDeniedException e) {
+                toThrow = e;
+                /* Workaround for https://bugs.openjdk.java.net/browse/JDK-8029608 */
+                try {
+                    Thread.sleep(10);
+                } catch (InterruptedException e1) {
+                    Thread.currentThread().interrupt();
+                    toThrow = e1;
+                }
+            } catch (IOException e) {
+                toThrow = e;
+            }
+        }
+        if (toThrow != null) {
+            throw new IOException(String.format("Could not create directory [%s]", dir), toThrow);
+        } else {
+            throw new IOException(
+                    String.format("Could not create directory [%s] attempting [%d] times", dir, CREATE_RETRY_COUNT));
+        }
+
+    }
+
+    /**
+     * Deletes a file or directory recursively if it exists.
+     *
+     * @param directory the directory to delete
+     * @throws IOException
+     */
+    static void deleteDirectory(Path directory) throws IOException {
+        if (Files.exists(directory)) {
+            Files.walkFileTree(directory, new SimpleFileVisitor<Path>() {
+                @Override
+                public FileVisitResult postVisitDirectory(Path dir, IOException exc) throws IOException {
+                    if (exc == null) {
+                        Files.delete(dir);
+                        return FileVisitResult.CONTINUE;
+                    } else {
+                        // directory iteration failed; propagate exception
+                        throw exc;
+                    }
+                }
+
+                @Override
+                public FileVisitResult visitFile(Path file, BasicFileAttributes attrs) throws IOException {
+                    if (isWindows) {
+                        final long deadline = System.currentTimeMillis() + DELETE_RETRY_MILLIS;
+                        FileSystemException lastException = null;
+                        do {
+                            try {
+                                Files.delete(file);
+                                return FileVisitResult.CONTINUE;
+                            } catch (FileSystemException e) {
+                                lastException = e;
+                            }
+                        } while (System.currentTimeMillis() < deadline);
+                        throw new IOException(String.format("Could not delete file %s after retrying for %d ms", file,
+                                DELETE_RETRY_MILLIS), lastException);
+                    } else {
+                        Files.delete(file);
+                    }
+                    return FileVisitResult.CONTINUE;
+                }
+
+                @Override
+                public FileVisitResult visitFileFailed(Path file, IOException exc) throws IOException {
+                    // try to delete the file anyway, even if its attributes
+                    // could not be read, since delete-only access is
+                    // theoretically possible
+                    Files.delete(file);
+                    return FileVisitResult.CONTINUE;
+                }
+            });
+        }
+    }
+
+    public static class LazyRecipeDirectory implements RecipeDirectory {
+
+        private final Future<RecipeDirectory> delegate;
+
+        public LazyRecipeDirectory(Future<RecipeDirectory> delegate) {
+            super();
+            this.delegate = delegate;
+        }
+
+        RecipeDirectory awaitRecipeDirectory() {
+            try {
+                return delegate.get();
+            } catch (InterruptedException e) {
+                Thread.currentThread().interrupt();
+                throw new RuntimeException(e);
+            } catch (ExecutionException e) {
+                throw new RuntimeException(e);
+            }
+        }
+
+        @Override
+        public Optional<RecipePathMatch> getArtifactPaths(String groupId, String artifactId, String version) {
+            return awaitRecipeDirectory().getArtifactPaths(groupId, artifactId, version);
+        }
+
+        @Override
+        public Optional<Path> getBuildPaths(String scmUri, String version) {
+            return awaitRecipeDirectory().getBuildPaths(scmUri, version);
+        }
+
+        @Override
+        public Optional<Path> getRepositoryPaths(String name) {
+            return awaitRecipeDirectory().getRepositoryPaths(name);
+        }
+
+        @Override
+        public Optional<Path> getBuildToolInfo(String name) {
+            return awaitRecipeDirectory().getBuildToolInfo(name);
+        }
+
+        @Override
+        public List<Path> getAllRepositoryPaths() {
+            return awaitRecipeDirectory().getAllRepositoryPaths();
+        }
+
+        @Override
+        public Optional<Path> getDisabledPlugins(String tool) {
+            return awaitRecipeDirectory().getDisabledPlugins(tool);
+        }
+
+        @Override
+        public void update() {
+            awaitRecipeDirectory().update();
+        }
+
+    }
+
 }
--- domino/build-recipes/src/test/java/com/redhat/hacbs/recipes/scm/GitScmLocatorTest.java
+++ domino/build-recipes/src/test/java/com/redhat/hacbs/recipes/scm/GitScmLocatorTest.java
@@ -1,9 +1,15 @@
 package com.redhat.hacbs.recipes.scm;
 
+import static org.assertj.core.api.Assertions.assertThat;
+
+import com.redhat.hacbs.recipes.BuildRecipe;
 import com.redhat.hacbs.recipes.GAV;
+import java.nio.file.Path;
 import java.util.Arrays;
 import java.util.HashMap;
+import java.util.List;
 import java.util.Map;
+import java.util.UUID;
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 
@@ -20,11 +26,48 @@ class GitScmLocatorTest {
 
     @Test
     void lookupScmInfoCommonsLang() {
-        TagInfo tag = GitScmLocator.builder().build().resolveTagInfo(GAV.parse("commons-lang:commons-lang:2.5"));
+        assertCommonsTag(GitScmLocator.builder().build());
+    }
+
+    private void assertCommonsTag(GitScmLocator locator) {
+        TagInfo tag = locator.resolveTagInfo(GAV.parse("commons-lang:commons-lang:2.5"));
         Assertions.assertNotNull(tag);
         Assertions.assertEquals(tag.getTag(), "LANG_2_5");
         Assertions.assertNotEquals(tag.getTag(), tag.getHash());
         Assertions.assertEquals(tag.getRepoInfo().getUri(), "https://github.com/apache/commons-lang.git");
+    }
+
+    @Test
+    void reuseWorkingCopy() {
+        final Path gitCloneDir = Path.of("target/GitScmLocatorTest/lookupScmInfoCommonsLang-" + UUID.randomUUID());
+        assertThat(gitCloneDir).doesNotExist();
+
+        final String repoUrl = BuildRecipe.DEFAULT_RECIPE_REPO_URL;
+        {
+            long t1 = System.currentTimeMillis();
+            final GitScmLocator locator = GitScmLocator.builder()
+                    .setRecipeRepos(List.of(repoUrl))
+                    .setGitCloneBaseDir(gitCloneDir)
+                    .build();
+            assertCommonsTag(locator);
+
+            System.out.println("t1 " + (System.currentTimeMillis() - t1));
+
+            assertThat(gitCloneDir).exists();
+            assertThat(gitCloneDir.resolve(
+                    GitScmLocator.uriToFileName(repoUrl) + "/.git")).exists();
+        }
+
+        // reuse the existing repo
+        {
+            long t1 = System.currentTimeMillis();
+            final GitScmLocator locator = GitScmLocator.builder()
+                    .setRecipeRepos(List.of(repoUrl))
+                    .setGitCloneBaseDir(gitCloneDir)
+                    .build();
+            assertCommonsTag(locator);
+            System.out.println("t2 " + (System.currentTimeMillis() - t1));
+        }
     }
 
     //test tag mapping heuristics
@@ -39,6 +82,21 @@ class GitScmLocatorTest {
         runPassingTest("1.0.Final", "1.0", "1.0", "1.0.a1");
         runFailingTest("1.0", "1.0.Beta1", "1.0.Alpha1");
         runFailingTest("1.0", "1.0.Final", "1.0.Alpha1");
+    }
+
+    @Test
+    void uriToFileName() {
+        assertThat(GitScmLocator.uriToFileName("https://github.com/path/to/report.pdf?download=1#section"))
+                .isEqualTo("github.com-path-to-report.pdf-download-1-section");
+        assertThat(GitScmLocator.uriToFileName("https://github.com/org/repo.git")).isEqualTo("github.com-org-repo");
+        assertThat(GitScmLocator.uriToFileName("file:///C:/Program Files/Some App/app.exe"))
+                .isEqualTo("C-Program-Files-Some-App-app.exe");
+        assertThat(GitScmLocator.uriToFileName("C:\\Program Files\\Some App\\app.exe"))
+                .isEqualTo("C-Program-Files-Some-App-app.exe");
+        assertThat(GitScmLocator.uriToFileName("git+ssh://git@github.com:owner/repo.git")).isEqualTo("github.com-owner-repo");
+        assertThat(GitScmLocator.uriToFileName("https://example.com/trailing-dot.")).isEqualTo("example.com-trailing-dot");
+        assertThat(GitScmLocator.uriToFileName("git@github.com:quarkusio/quarkus.git"))
+                .isEqualTo("github.com-quarkusio-quarkus");
     }
 
     void runPassingTest(String version, String expected, String... tags) {
